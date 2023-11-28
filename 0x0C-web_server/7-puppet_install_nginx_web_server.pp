3047d6ba4de9b060d9dfb8d2a97edbec9e7df375
kdshola <khidirshola@gmail.com>
2023-11-28 18:18:20 +0100

    updage scripts

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # Install and confiture an Nginx server
 
 exec { 'install and configure':
-command  => 'sudo apt-get -y update; sudo apt-get -y upgrade; sudo apt-get -y install nginx; sudo ufw allow 'Nginx HTTP'; echo 'Hello World!' > /var/www/html/index.nginx-debian.html; sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default; sudo service nginx start',
+command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; sudo ufw allow 'Nginx HTTP'; echo 'Hello World!' > /var/www/html/index.nginx-debian.html; sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/ permanent;" /etc/nginx/sites-available/default; sudo service nginx start',
 provider => shell
 }
